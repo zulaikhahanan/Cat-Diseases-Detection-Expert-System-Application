ac3f639104c5686937ffc411f0cdb44d7568a45b
zulaikhahanan <aikhx00@gmail.com>
2024-12-01 14:40:28 +0800

    New Change

--- backend/models/Cat.js
+++ backend/models/Cat.js
@@ -1,2 +1,12 @@
 //backend/models/Cat.js
 
+const mongoose = require('mongoose');
+
+const diseaseSchema = new mongoose.Schema({
+    name: String,
+    symptoms: [String],
+    description: String,
+    treatment: String
+});
+
+module.exports = mongoose.model('Disease', diseaseSchema);
